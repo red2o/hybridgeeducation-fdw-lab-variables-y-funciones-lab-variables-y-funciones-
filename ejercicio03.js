ec30230720f55d6c3191bf0e3addfc057a03db7c
red2o <oscarrdjc@gmail.com>
2025-11-09 12:50:51 -0600

    Update ejercicio03.js

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,10 +1,9 @@
 // Ejercicio 3: Suma de Dos Números
 // Declara una función llamada "sumar" que acepte dos parámetros (a y b) y retorne su suma
+function sumar(a, b) {
+  return a + b;
+}
 
-//Escribe tu función aquí
-
-
-// Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
-    sumar
+  sumar
 };
