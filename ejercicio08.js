7af47a94e0eec0e088b678dc8d4f96977108e32f
red2o <oscarrdjc@gmail.com>
2025-11-09 13:03:59 -0600

    Update ejercicio08.js

--- ejercicio08.js
+++ ejercicio08.js
@@ -6,7 +6,7 @@
 // concatenarCadenas("Frase", "de prueba") => "Frasede prueba"
 
 function concatenarCadenas(cadena1, cadena2) {
-  return cadena1 + " " + cadena2;
+  return cadena1 + cadena2;
 }
 
 module.exports = {
