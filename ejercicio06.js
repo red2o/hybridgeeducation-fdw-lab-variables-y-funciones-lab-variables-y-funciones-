1f96a660206088d28d7e2563cec0bbc9a9f35db8
red2o <oscarrdjc@gmail.com>
2025-11-09 12:53:30 -0600

    Update ejercicio06.js

--- ejercicio06.js
+++ ejercicio06.js
@@ -1,10 +1,9 @@
 // Ejercicio 6: Conversión de Grados Celsius a Fahrenheit
 // Declara una función llamada "celsiusAFahrenheit" que acepte un parámetro "celsius" y retorne la conversión a Fahrenheit
+function celsiusAFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
 
-//escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "celsiusAFahrenheit"
-
-
-// Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
-    celsiusAFahrenheit
+  celsiusAFahrenheit
 };
