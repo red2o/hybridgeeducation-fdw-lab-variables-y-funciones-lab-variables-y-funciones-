22c0299939145f7e951b1205cb413b14a8b673b2
red2o <oscarrdjc@gmail.com>
2025-11-09 12:53:41 -0600

    Update ejercicio07.js

--- ejercicio07.js
+++ ejercicio07.js
@@ -1,10 +1,9 @@
 // Ejercicio 7: Comprobación de Número Par
 // Declara una función llamada "esPar" que acepte un parámetro "numero" y retorne verdadero si el número es par, de lo contrario falso
+function esPar(numero) {
+  return numero % 2 === 0;
+}
 
-//escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "esPar"
-
-
-// Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
-    esPar
+  esPar
 };
